1e86996d43b25b4fbb4a554a967f2371da3b6848
ylor <2200609+ylor@users.noreply.github.com>
2019-04-23 00:03:30 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -50,7 +50,7 @@ if [ $(uname -s) = "Linux" ]; then
         sudo apt install $pkg -y
       done
     echo "Installing Homebrew..."
-    git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
     test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
     test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
     echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
