88dc0d58736047c53a25c7f438413cd201eeb9f7
ylor <2200609+ylor@users.noreply.github.com>
2019-04-23 07:56:01 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -56,8 +56,8 @@ if [ $(uname -s) = "Linux" ]; then
     git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
     mkdir ~/.linuxbrew/bin
     ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
-    eval $(~/.linuxbrew/bin/brew shellenv)
-    brew update && brew upgrade && brew cleanup
+    #eval $(~/.linuxbrew/bin/brew shellenv)
+    #brew update && brew upgrade && brew cleanup
     #add ~/.local/bin to PATH
   fi
   fish
