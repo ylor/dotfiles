6997be15152de3f5389773b40c3f50667c464036
ylor <2200609+ylor@users.noreply.github.com>
2019-04-23 01:41:06 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@
 # Ask for the administrator password upfront
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Prestage folders for symlinking
 mkdir "$HOME/.config"
 mkdir -p "$HOME/.config/fish"
@@ -25,15 +28,11 @@ if [ $(uname -s) = "Darwin" ]; then
   xcode-select --install
   sudo xcodebuild -license accept
 
-  # install brew
+  # Install Homebrew, if not installed
     if [ ! -f /usr/local/bin/brew ]; then
-        echo "Installing Homebrew.. "
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
-    brew update
-    brew upgrade
-    brew bundle
-    brew cleanup
+    brew update && brew upgrade && brew bundle && brew cleanup
     
     # Change shell to fish if it's not already that
     if ! [ $(cat /etc/shells | grep fish) ]; then
@@ -41,7 +40,7 @@ if [ $(uname -s) = "Darwin" ]; then
     fi
 fi
 
-# If Linux do below
+# If on Linux below
 if [ $(uname -s) = "Linux" ]; then
   # Check for apt on Debian/Ubuntu
   if [ -f /usr/bin/apt ]; then
@@ -54,15 +53,13 @@ if [ $(uname -s) = "Linux" ]; then
     chsh -s $(which fish)
   fi
   
+  # If Homebrew is not installed, install it
   if ! [ -d ~./linuxbrew ]; then
-    echo "Installing Homebrew..."
     git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
     mkdir ~/.linuxbrew/bin
     ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
     eval $(~/.linuxbrew/bin/brew shellenv)
-    brew update
-    brew upgrade
-    brew cleanup
+    brew update && brew upgrade && brew cleanup
     #add ~/.local/bin to PATH
   fi
 fi
