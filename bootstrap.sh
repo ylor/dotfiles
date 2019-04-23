3fd8051cbd6dc9ccfd3e8354f0bd70c6e59c3138
ylor <2200609+ylor@users.noreply.github.com>
2019-04-23 00:12:54 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,7 +53,6 @@ if [ $(uname -s) = "Linux" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
     test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
     test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
     echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
     # Change shell to fish
     #echo (which fish) | sudo tee -a /etc/shells
