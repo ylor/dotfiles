e3889c484020029bc1a8a6835ebbb1d2a9c40bfd
ylor <2200609+ylor@users.noreply.github.com>
2019-04-23 19:37:54 -0400

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,1 +1,18 @@
+# Ask for the administrator password upfront
+#sudo -v
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+  # Check for apt on Debian/Ubuntu
+  if [ -f /usr/bin/apt ]; then
+    sudo apt update
+    sudo apt upgrade
+    packages="build-essential curl exa fd-find file fish gnome-tweaks git nodejs npm ripgrep"
+    for pkg in $packages
+      do
+        sudo apt install $pkg -y
+      done
+    sudo apt autoremove -y
+    chsh -s $(which fish)
+  fi
+  fish
