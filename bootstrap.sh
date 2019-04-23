7b8718492c1564a4d2ad2b8c0b6a16bdd095f8b1
ylor <2200609+ylor@users.noreply.github.com>
2019-04-23 18:34:05 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,52 +13,19 @@ mkdir "$HOME/.ssh"
 # Symlinks
 dot="bash_profile bashrc bin config/fish/conf.d config/mpv hushlogin ssh/config tvnamer"
 for file in $dot; do
-	echo "Symlinking $file"
-	ln -s "$(pwd)/$file" "$HOME/.$file"
+    echo "Symlinking $file"
+    ln -s "$(pwd)/$file" "$HOME/.$file"
 done
 
-# if macOS run below
-if [ $(uname -s) = "Darwin" ]; then
-  # get the command line tools and accept the license
-  xcode-select --install
-  sudo xcodebuild -license accept
-
-  # Install Homebrew, if not installed
-    if [ ! -f /usr/local/bin/brew ]; then
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-    brew update && brew upgrade && brew bundle && brew cleanup
-    
-    # Change shell to fish if it's not already that
-    if ! [ $(cat /etc/shells | grep fish) ]; then
-      echo $(which fish) | sudo tee -a /etc/shells && chsh -s $(which fish)
-    fi
-    
-    # macOS defaults
+# Check for operating system
+case $(uname) in
+  'Darwin') 
     source macos.sh
-fi
-
-# If on Linux below
-if [ $(uname -s) = "Linux" ]; then
-  # Check for apt on Debian/Ubuntu
-  if [ -f /usr/bin/apt ]; then
-    packages="build-essential curl file fish gnome-tweaks git"
-    for pkg in $packages
-      do
-        sudo apt install $pkg -y
-      done
-    sudo apt autoremove -y
-    chsh -s $(which fish)
-  fi
-  
-  # If Homebrew is not installed, install it
-  if ! [ -d ~./linuxbrew ]; then
-    git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-    mkdir ~/.linuxbrew/bin
-    ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
-    #eval $(~/.linuxbrew/bin/brew shellenv)
-    #brew update && brew upgrade && brew cleanup
-    #add ~/.local/bin to PATH
-  fi
-  fish
-fi
+    ;;
+  'Linux')
+    source linux.sh
+    ;;
+   *)        
+    echo "Unknown operating system. Aborting script." 
+    ;;
+esac
