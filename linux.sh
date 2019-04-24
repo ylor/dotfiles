51f21427cea70885c6d7f8ce4f886f6cd9fee5c8
ylor <2200609+ylor@users.noreply.github.com>
2019-04-23 20:57:09 -0400

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-# Ask for the administrator password upfront
-#sudo -v
-# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-  # Check for apt on Debian/Ubuntu
+# Check for apt on Debian/Ubuntu
 if [ -f /usr/bin/apt ]; then
   sudo apt update
   sudo apt upgrade -y
@@ -14,6 +10,4 @@ if [ -f /usr/bin/apt ]; then
       sudo apt install $pkg -y
     done
   sudo apt autoremove -y
-  #chsh -s $(which fish)
 fi
-fish
