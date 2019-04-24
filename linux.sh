f8be29a113543b211ce392bd18658c507eedb7e7
ylor <2200609+ylor@users.noreply.github.com>
2019-04-23 20:35:00 -0400

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -5,15 +5,15 @@
 #while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
   # Check for apt on Debian/Ubuntu
-  if [ -f /usr/bin/apt ]; then
-    sudo apt update
-    sudo apt upgrade
-    packages="build-essential curl exa fd-find file fish gnome-tweaks git nodejs npm ripgrep"
-    for pkg in $packages
-      do
-        sudo apt install $pkg -y
-      done
-    sudo apt autoremove -y
-    chsh -s $(which fish)
-  fi
-  fish
+if [ -f /usr/bin/apt ]; then
+  sudo apt update
+  sudo apt upgrade
+  packages="build-essential curl exa fd-find file fish gnome-tweaks git nodejs npm ripgrep"
+  for pkg in $packages
+    do
+      sudo apt install $pkg -y
+    done
+  sudo apt autoremove -y
+  #chsh -s $(which fish)
+fi
+fish
