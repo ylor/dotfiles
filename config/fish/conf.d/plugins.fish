c4a4c1d151814e759bfa9a03c3cf47b90493c759
ylor <2200609+ylor@users.noreply.github.com>
2019-04-30 20:33:55 -0400

    Update plugins.fish

--- config/fish/conf.d/plugins.fish
+++ config/fish/conf.d/plugins.fish
@@ -1,12 +1,12 @@
+# Source Functions
+[ -d ~/.config/fish/functions ]; and for function in ~/.config/fish/functions/*.fish; source $function; end
+
 # Bootstrap fisher if not installed
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
-
-# Source Functions
-[ -d ~/.config/fish/functions ]; and for function in ~/.config/fish/functions/*.fish; source $function; end
 
 # Source Autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
