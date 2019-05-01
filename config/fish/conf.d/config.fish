89b987757b17cf5da3a8cba0e71d12ea4fca34ad
ylor <2200609+ylor@users.noreply.github.com>
2019-05-01 11:52:56 -0400

    Update config.fish

--- config/fish/conf.d/config.fish
+++ config/fish/conf.d/config.fish
@@ -53,44 +53,42 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
-alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
-alias cdf="cd (pfd)"
+
 alias dotpull="git -C ~/Documents/dotfiles pull; exec fish"
-alias dscleanup="sudo find / -name '*.DS_Store' -type f -ls -delete"
-alias etrash="sudo rm -rfv /Volumes/*/.Trashes; and sudo rm -rfv ~/.Trash; and sudo rm -rfv /private/var/log/asl/*.asl"
-alias find="fd"
-alias flush="dscacheutil -flushcache; and sudo killall -HUP mDNSResponder"
-alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false; and killall Finder"
+
 alias home="cd $HOME"
 alias http="python -m SimpleHTTPServer"
 alias j="z"
-alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user; and killall Finder"
+
 alias md="mkdir -p"
-alias mkdir="mkdir -p"
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias reboot="sudo shutdown -r now"
 alias reload="exec fish"
-alias show="defaults write com.apple.finder AppleShowAllFiles -bool true; and killall Finder"
+
 alias shutdown="sudo shutdown -s now"
-alias vnc="open vnc://Server.local"
 alias rd="rmdir"
 
 # Functions
 
 ## Quality of Life
+
+### git clone && cd to it
 function gc
-  git clone --recurse-submodules "$argv"; and cd (basename $argv)
+  git clone --recurse-submodules "$argv" && cd (basename $argv)
 end
 
+### Nix
 alias nixi="nix-env -i"
 alias nixu="nix-env -u"
 alias nixun="nix-env --uninstall"
 alias nixup="nix-env --upgrade"
 
+### mkdir & cd to it
 function mdcd
-  mkdir $argv; and cd $argv
+  mkdir $argv && cd $argv
 end
 
+### Easy Updates
 function update -d "Update important software"
   echo (set_color white --bold)"// Homebrew"(set_color normal); and brew update; and brew upgrade; brew cleanup; brew cask cleanup
   echo (set_color white --bold)"// dotfiles"(set_color normal); and git -C ~/Documents/dotfiles/ pull
@@ -106,26 +104,41 @@ function update-full -d "Update all software"
 end
 
 ## Utility Replacements if available
-if command -vq rg > /dev/null 2>&1
+if command -v fd > /dev/null 2>&1
+  alias find="fd"
+end
+
+if command -v rg > /dev/null 2>&1
   alias grep="rg"
 end
  
-if command -vq exa
+if command -v exa > /dev/null 2>&1
   alias la="exa -la"
   alias ll="exa -l"
   alias ls="exa"
 end
 
-if command -vq htop
+if command -v htop > /dev/null 2>&1
   alias top="htop"
 end
 
-if command -vq nvim
+if command -v nvim > /dev/null 2>&1
   alias vi="nvim"
   alias vim="nvim"
 end
 
 ## Mac Specific
-function cdf
-  cd (osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)')
+if (uname) = Darwin > /dev/null 2>&1
+  alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+  alias dscleanup="sudo find / -name '*.DS_Store' -type f -ls -delete"
+  alias etrash="sudo rm -rfv /Volumes/*/.Trashes; and sudo rm -rfv ~/.Trash; and sudo rm -rfv /private/var/log/asl/*.asl"
+  alias flush="dscacheutil -flushcache; and sudo killall -HUP mDNSResponder"
+  alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false; and killall Finder"
+  alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user; and killall Finder"
+  alias show="defaults write com.apple.finder AppleShowAllFiles -bool true; and killall Finder"
+  alias vnc="open vnc://Server.local"
+  
+  function cdf
+    cd (osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)')
+  end
 end
