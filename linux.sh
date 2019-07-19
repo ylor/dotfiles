79e9aa495f0a1d4205df7138ccfbe12057e29d40
Roly <roly@localhost.localdomain>
2019-07-18 20:53:22 -0400

    multiline var

--- linux.sh
+++ linux.sh
@@ -7,29 +7,32 @@ if [ -f /usr/bin/apt ]; then
   sudo apt install build-essential curl -y
 
   # Add custom repositories
+  ## UI
   sudo add-apt-repository ppa:daniruiz/flat-remix -y
+  ## VSCode
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
+  sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  ## Yarn
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
   sudo apt update
   sudo apt upgrade -y
-  packages=(
-    exa
-    fd-find
-    fish
-    flat-remix flat-remix-gnome flat-remix-gtk
-    git
-    gnome-tweaks
-    libssl-devel
-    nodejs
-    pkg-config
-    ripgrep
-    rustc cargo
-    stow
-    yarn
+  pkgs=(
+    "exa"
+    "fd-find"
+    "fish"
+    "flat-remix flat-remix-gnome flat-remix-gtk"
+    "git"
+    "gnome-tweaks"
+    "nodejs"
+    "ripgrep"
+    "rustc cargo libssl-devel pkg-config"
+    "stow"
+    "yarn"
   )
-  for pkg in $packages; do
-    sudo apt install "$pkg" -y
-  done
+  sudo apt install ${pkgs[@]} -y
   sudo apt autoremove -y
 fi
 
@@ -48,22 +51,20 @@ if [ -f /usr/bin/dnf ]; then
   sudo dnf update
   sudo dnf upgrade -y
   sudo dnf groupinstall "Development Tools"
-  packages=(
-    chsh
-    exa
-    fd-find
-    fish
-    flat-remix flat-remix-gnome flat-remix-gtk
-    gnome-tweaks
-    openssl-devel
-    ripgrep
-    rust cargo
-    stow
-    yarn
+  pkgs=(
+    "chsh"
+    "exa"
+    "fd-find"
+    "fish"
+    "flat-remix flat-remix-gnome flat-remix-gtk"
+    "gnome-tweaks"
+    "openssl-devel"
+    "ripgrep"
+    "rust cargo"
+    "stow"
+    "yarn"
   )
-  for pkg in $packages; do
-    sudo dnf install "$pkg" -y
-  done
+  sudo dnf install ${pkgs[@]} -y
 
   sudo curl -sL https://rpm.nodesource.com/setup_12.x | bash -
 fi
