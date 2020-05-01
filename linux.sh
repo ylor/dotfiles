13303721b295cf84488c5064b2cd95f483b3a7ee
Roly <roly@server.lan>
2020-05-01 16:08:54 -0400

    even more git stuff

--- linux.sh
+++ linux.sh
@@ -41,12 +41,6 @@ if [ -f /usr/bin/dnf ]; then
   ## RPM Fusion
   sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-<<<<<<< HEAD
-=======
-  ## Flatpak
-  sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
->>>>>>> e8fc7ea41f7eeb0cad9477da89747c94697ae58c
   ## VSCode -
   sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
   sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
