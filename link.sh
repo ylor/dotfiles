876f3b5177d8b348823ebd3f82eba27743b55faa
Roly <roly@server.lan>
2020-05-06 16:49:58 -0400

    make missing folders before linking

--- link.sh
+++ link.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
+folders=$(find "$PWD" \
+-type d \
+-not -path "$PWD" \
+-not -path "*.git*")
 files=$(find "$PWD" \
 -type f \
 -not -path "*.git*" \
 -not -path "./*.sh" \
 -not -path "*.DS_Store" | sort)
+
+mkdir -p "$(echo "$folders" | sed "s|$PWD|$HOME|")"
 
 for file in $files; do
     ln -sfnv "$file" "$(echo "$file" | sed "s|$PWD|$HOME|")"
