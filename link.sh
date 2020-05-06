558448224bb8d6e8618c74e21f15a98b0aebb16b
Roly <roly@server.lan>
2020-05-06 18:42:29 -0400

    mkdirp test 2

--- link.sh
+++ link.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env sh
-folders=$(find "$PWD" \
--type d \
--not -path "$PWD" \
--not -path "*.git*")
+mkdir -p "$(find "$PWD" -type d -not -path "$PWD" -not -path "*.git*" | sed "s|$PWD|$HOME|")"
+
 files=$(find "$PWD" \
 -type f \
 -not -path "*.git*" \
 -not -path "./*.sh" \
 -not -path "*.DS_Store" | sort)
-
-mkdir -p "$("$folders" | sed "s|$PWD|$HOME|")"
-
 for file in $files; do
     ln -sfnv "$file" "$(echo "$file" | sed "s|$PWD|$HOME|")"
 done
