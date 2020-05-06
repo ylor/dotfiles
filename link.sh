428fd61a2f855ed714536ef03764fec1571db74d
Roly <roly@server.lan>
2020-05-06 16:20:42 -0400

    Change . => $PWD so symlinks resolve

--- link.sh
+++ link.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
-files=$(find . \
+files=$(find "$PWD" \
 -type f \
--not -path "*.git/*" \
+-not -path "*.git*" \
 -not -path "./*.sh" \
 -not -path "*.DS_Store" | sort)
 
 for file in $files; do
-    ln -sfnv "$file" $(echo $file | sed "s|\.|${HOME}|")
+    ln -sfnv "$file" "$(echo "$file" | sed "s|$PWD|$HOME|")"
 done
