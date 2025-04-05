1fbf18ace27e009d9e4e40dd8051f5a946b2a6c6
ylor <2200609+ylor@users.noreply.github.com>
2025-04-05 00:26:44 -0400

    I am Spartacus

--- link.sh
+++ link.sh
@@ -2,29 +2,19 @@
 
 # Clean up broken symlinks
 find -L "$HOME/.config" -type l -exec rm {} \;
-# Find all necesssary folders that symlinks succeed
-folders=$(
-    find ${PWD} \
-        -type d \
-        -mindepth 1 \
-        -not -path "*.git*" | sed "s_${PWD}_${HOME}_"
-)
-# Make those necessary directories
-for folder in ${folders}; do
-    mkdir -pv "${folder}"
-done
+find -L "$HOME/.local" -type l -exec rm {} \;
 
-# Find all dotfiles we'd like to symlink
-files=$(
-    find "${PWD}" \
-        -type f \
-        -not -name "Brewfile*" \
-        -not -name "*.DS_Store" \
-        -not -path "*.git*" \
-        -not -name "*.sh"
-)
-# Symlink them
-for source in ${files}; do
-    target=$(echo "${source}" | sed "s_${PWD}_${HOME}_")
-    ln -sfnv "$source" "$target"
-done
+# Find all necesssary folders that symlinks succeed
+     find "${PWD}" \
+    -type d \
+    -mindepth 2 \
+    -not -path "*.git*" |  while read folder; do
+        mkdir -pv "$(echo "${folder}" | sed "s_${PWD}_${HOME}_")"
+    done
+    
+find "${PWD}" \
+    -type f \
+    -mindepth 2 \
+    -not -path "*.git*" | while read file; do
+        ln -sfnv "$file" "$(echo "${file}" | sed "s_${PWD}_${HOME}_")"
+    done
