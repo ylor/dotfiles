dd348588f7316e56c46f88c492cb3ba1f9ee86b9
ylor <2200609+ylor@users.noreply.github.com>
2025-04-05 00:39:00 -0400

    Added a banner to the default admin page. Please have mercy on me =(

--- link.sh
+++ link.sh
@@ -8,13 +8,9 @@ find -L "$HOME/.local" -type l -exec rm {} \;
 find "${PWD}" \
 	-type d \
 	-mindepth 2 \
-	-not -path "*.git*" | while read folder; do
-	mkdir -pv "$(echo "${folder}" | sed "s_${PWD}_${HOME}_")"
-done
+	-not -path "*.git*" | while read folder; do mkdir -pv "$(echo "${folder}" | sed "s_${PWD}_${HOME}_")"; done
 
 find "${PWD}" \
 	-type f \
 	-mindepth 2 \
-	-not -path "*.git*" | while read file; do
-	ln -sfnv "$file" "$(echo "${file}" | sed "s_${PWD}_${HOME}_")"
-done
+	-not -path "*.git*" | while read file; do ln -sfnv "$file" "$(echo "${file}" | sed "s_${PWD}_${HOME}_")"; done
