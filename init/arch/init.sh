c45457cdb36d2a2d088d6fb9751d534f4157f9b4
ylor <2200609+ylor@users.noreply.github.com>
2025-04-09 18:42:06 -0400

    The dog is eating my code

--- init/arch/init.sh
+++ init/arch/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # https://github.com/typecraft-dev/crucible
 
-pacman -S --noconfirm fish gum
+sudo gum spin --title="waka waka" -- pacman -S --noconfirm fish gum
 
 gum spin --title="guh-nome" && if exist gsettings; then
   gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
