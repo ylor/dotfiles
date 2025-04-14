ec5ea810cbdb4843a497ca7f86bcd6ed0ff9ee1a
ylor <2200609+ylor@users.noreply.github.com>
2025-04-14 17:41:02 -0400

    holy shit it's functional

--- link.sh
+++ link.sh
@@ -15,11 +15,20 @@ done
 
 # find and symlink dotfiles
 find "$home" -type f | while read file; do
-	ln -sfnv "$file" "$(rehome "$file")"
+	ln -sfn "$file" "$(rehome "$file")"
 done
 
+# purge broken symlinks
+find -L "$HOME" -type l -maxdepth 1 -exec rm {} \+
+find -L "$HOME/.config" "$HOME/.local" -type l -exec rm {} \+
+
+# purge empty folders
+find "$HOME" -type d -maxdepth 1 -empty -delete
+find "$HOME/.config" "$HOME/.local" -type d -empty -delete
+
+
+# rm broken symlinks
+# find "$HOME" "$HOME/.config" "$HOME/.local" -type d -maxdepth 1 -empty
+# find -L "$HOME" -path "$HOME"/Library -prune -or -type l -exec rm {} \+ 2>/dev/null || true
 # delete empty folders
 # find "$HOME" -name "Library" -prune -or -mindepth 1 -type d -empty -delete 2>/dev/null || true
-# rm broken symlinks
-find -L "$HOME" -path "$HOME"/Library -prune -or -type l -exec rm {} \+ 2>/dev/null || true
-# find -L "$HOME/.config" "$HOME/.local" -type l -exec rm {} \+
