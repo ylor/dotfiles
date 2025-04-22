a7cf2c49c244acdc2ef94abf0b16e81e5ae0ba54
ylor <2200609+ylor@users.noreply.github.com>
2025-04-22 11:55:16 -0400

    bug fix

--- link.fish
+++ link.fish
@@ -1,28 +1,25 @@
 #!/bin/env fish
 # dotfile "management"
-set home "$(status dirname)/home"
+set home (realpath "$(status dirname)/home")
 
-echo $home
-exit
+function rehome
+	string replace "$home" "$HOME" "$argv"
+end
 
-# rehome() {
-# 	echo "$1" | sed "s_${home}_${HOME}_g"
-# }
+# find and stage folders for symlinks
+find "$home" -type d | while read folder
+	mkdir -pv "$(rehome "$folder")"
+end
 
-# # find and stage folders for symlinks
-# find "$home" -type d | while read folder; do
-# 	mkdir -pv "$(rehome "$folder")"
-# done
+# find and symlink dotfiles
+find "$home" -type f | while read file
+    ln -sfv "$file" "$(rehome "$file")"
+end
 
-# # find and symlink dotfiles
-# find "$home" -type f | while read file; do
-# 	ln -sfv "$file" "$(rehome "$file")"
-# done
+# purge broken symlinks
+find -L "$HOME" -type l -maxdepth 1 -exec rm {} \+
+find -L "$HOME/.config" "$HOME/.local" -type l -exec rm {} \+
 
-# # purge broken symlinks
-# find -L "$HOME" -type l -maxdepth 1 -exec rm {} \+
-# find -L "$HOME/.config" "$HOME/.local" -type l -exec rm {} \+
-
-# # purge empty folders
-# find "$HOME" -type d -maxdepth 1 -empty -delete
-# find "$HOME/.config" "$HOME/.local" -type d -empty -delete
+# purge empty folders
+find "$HOME" -type d -maxdepth 1 -empty -delete
+find "$HOME/.config" "$HOME/.local" -type d -empty -delete
