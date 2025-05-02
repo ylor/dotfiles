dda3ff1f6679ae1d82ecb8e77551aae3158e3824
ylor <2200609+ylor@users.noreply.github.com>
2025-05-01 22:22:11 -0400

    pushing another fix

--- init/linux/gnome.fish
+++ init/linux/gnome.fish
@@ -1,7 +1,7 @@
-#!/bin/sh
-set -eu
+#!/bin/env fish
 
 if exist gsettings
 	gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 	gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
-end and echo "you got gnomed!"
+end
+and echo "you got gnomed!"
