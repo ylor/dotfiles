697de0d1c7bd2c84a68a863d56f7b0b03d05d76a
ylor <2200609+ylor@users.noreply.github.com>
2025-05-12 09:42:54 -0400

    lets drink beer

--- init/linux/arch.fish
+++ init/linux/arch.fish
@@ -1,4 +1,15 @@
 if exist pacman
-    missing gum && sudo pacman -S --noconfirm gum
+    sudo pacman -Syu
+    sudo pacman -S --noconfirm gum
+    
+    if exist gsettings
+        echo GNOME
+    else if exist hyprland
+        echo hyprland
+    else
+        gum choose GNOME Hyprland
+    end
+
+    sudo pacman -S --noconfirm kitty hyprland
     sudo gum spin --title="waka waka" -- sleep 5
 end
---
+++ init/linux/hyprland.fish
@@ -0,0 +1,4 @@
+if exist hyprland
+    sudo pacman -Syu --noconfirm
+    sudo pacman -S kitty hyprland nautilus rofi waybar
+end
