00a7af5842661901701617eec0ae5657e8009660
ylor <2200609+ylor@users.noreply.github.com>
2025-08-10 13:38:28 -0400

    Fix the fixes

--- init/darwin/macos.fish
+++ init/darwin/macos.fish
@@ -1,5 +1,5 @@
 # Configure dock
-if defaults read com.apple.Dock | grep -q "com.apple.TV"
+if defaults read com.apple.Dock | grep -q "com.apple.apps.launcher"
     command -vq dockutil || brew install --quiet dockutil
     dockutil --remove all --add /Applications --add "$HOME/Downloads" >/dev/null
 end
