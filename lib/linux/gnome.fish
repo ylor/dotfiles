d91af792a5eab1944112ffb0691a74baee8f20f7
ylor <2200609+ylor@users.noreply.github.com>
2025-10-12 16:30:50 -0400

    holy shit it's functional

--- lib/linux/gnome.fish
+++
@@ -1,7 +0,0 @@
-if command -vq gsettings
-    gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-    gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-    gsettings set org.gnome.desktop.interface icon-theme "Yaru-blue"
-    
-    sudo gtk-update-icon-cache /usr/share/icons/Yaru
-end
