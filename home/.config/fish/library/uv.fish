83b7a68a04afe40fee8a04c0cae42240ed03d4da
ylor <2200609+ylor@users.noreply.github.com>
2025-11-06 12:20:00 -0500

    git stash *

--- home/.config/fish/library/uv.fish
+++ home/.config/fish/library/uv.fish
@@ -3,11 +3,7 @@ if command -vq uv
     uv generate-shell-completion fish | source
     uvx --generate-shell-completion fish | source
 end
-<<<<<<< Updated upstream
 
 # function handler --on-event fish_postexec
 #     echo generator is done
-||||||| Stash base
-=======
->>>>>>> Stashed changes
 # end
