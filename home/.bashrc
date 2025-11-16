7a0a7625e4ca94ab183ed387b8bb207e051c117b
ylor <2200609+ylor@users.noreply.github.com>
2025-11-16 18:43:04 -0500

    a lot of shit

--- home/.bashrc
+++ home/.bashrc
@@ -1,3 +1,10 @@
+# environment variable for nvidia use
+if [[ $(lspci | grep -i 'nvidia') ]]; then
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export LIBVA_DRIVER_NAME=nvidia
+    export NVD_BACKEND=direct
+fi
+
 # drop into fish for interactive shells
 # adapted from https://wiki.archlinux.org/title/Fish
 if [[ $(command -v fish) && "$(ps -p "$PPID" -o comm=)" != "fish" && -z ${BASH_EXECUTION_STRING} ]]; then
--- linux.fish
+++ linux.fish
@@ -1,1 +1,21 @@
-echo darwin
+# PACKAGES
+pacman linux linux-firmware
+# gaming - multilib, steam, umu-launcher, faugus-launcher, lact, sunshine, cuda
+
+# POWER MANAGEMENT
+if upower --enumerate | grep BAT
+    powerprofilesctl set balance
+else
+    powerprofilesctl set performance
+end
+
+# if ls /sys/class/power_supply/BAT* &>/dev/null; then
+#   # This computer runs on a battery
+#   powerprofilesctl set balanced || true
+
+#   # Enable battery monitoring timer for low battery notifications
+#   systemctl --user enable --now omarchy-battery-monitor.timer
+# else
+#   # This computer runs on power outlet
+#   powerprofilesctl set performance || true
+# fi
