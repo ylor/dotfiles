ea72bf9d2f08ce782d08576b6ff85f136bb5dabd
ylor <2200609+ylor@users.noreply.github.com>
2025-11-16 23:25:19 -0500

    Completed with no bugs...

--- home/.bashrc
+++ home/.bashrc
@@ -5,6 +5,14 @@ if [[ $(lspci | grep -i 'nvidia') ]]; then
     export NVD_BACKEND=direct
 fi
 
+if uwsm check may-start && uwsm select; then
+	exec uwsm start default
+fi
+
+# if uwsm check may-start; then
+#     exec uwsm start hyprland-uwsm.desktop
+# fi
+
 # drop into fish for interactive shells
 # adapted from https://wiki.archlinux.org/title/Fish
 if [[ $(command -v fish) && "$(ps -p "$PPID" -o comm=)" != "fish" && -z ${BASH_EXECUTION_STRING} ]]; then
--- linux.fish
+++ linux.fish
@@ -1,21 +1,62 @@
+sudo true
 # PACKAGES
-pacman linux linux-firmware
-# gaming - multilib, steam, umu-launcher, faugus-launcher, lact, sunshine, cuda
+# sudo pacman -Syu
+
+# AUR 
+if not command -vq paru
+    paru -S --needed base-devel
+    git clone https://aur.archlinux.org/paru-bin.git /tmp/paru-bin
+    pushd /tmp/paru-bin
+    makepkg -si --noconfirm
+    popd
+    rm -rf /tmp/paru-bin
+end
 
 # POWER MANAGEMENT
+paru -S --noconfirm --needed power-profiles-daemon zram-generator
 if upower --enumerate | grep BAT
     powerprofilesctl set balance
 else
     powerprofilesctl set performance
 end
 
-# if ls /sys/class/power_supply/BAT* &>/dev/null; then
-#   # This computer runs on a battery
-#   powerprofilesctl set balanced || true
+paru -S --needed --noconfirm brightnessctl ddcutil fprintd fwupd
+paru -S --needed --noconfirm ddcci-driver-linux-dkms
 
-#   # Enable battery monitoring timer for low battery notifications
-#   systemctl --user enable --now omarchy-battery-monitor.timer
-# else
-#   # This computer runs on power outlet
-#   powerprofilesctl set performance || true
-# fi
+# NETWORK
+paru -S --needed --noconfirm bluez bluez-utils iwd networkmanager
+sudo systemctl enable bluetooth.service
+
+# FIREWALL
+paru -S --needed --noconfirm ufw
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow ssh
+sudo ufw enable
+sudo ufw reload
+sudo systemctl enable ufw
+
+# AUDIO
+paru -S --needed --noconfirm pipewire pipewire-alsa pipewire-jack pipewire-pulse libpulse wireplumber
+
+# SECURE BOOT
+# paru -S --needed --noconfirm sbctl
+
+# TERMINAL
+paru -S --needed --noconfirm fish fd git gum
+paru -S --needed --noconfirm btop bat eza evil-helix-bin podman podman-compose viu vim wget unzip zoxide
+
+# DESKTOP
+paru -S --needed --noconfirm
+paru -S --needed --noconfirm uwsm hyprland hypridle hyprlock hyprshot
+paru -S --needed --noconfirm polkit-gnome xwayland-satellite qt5-wayland qt6-wayland xdg-desktop-portal-gnome xdg-desktop-portal-gtk xdg-desktop-portal-hyprland
+paru -S --needed --noconfirm waybar otf-font-awesome
+paru -S --needed --noconfirm noctalia-shell cava mutagen-git gpu-screen-recorder
+paru -S --needed --noconfirm ttf-iosevka-aile ttf-jetbrains-mono-nerd
+paru -S --needed --noconfirm adw-gtk-theme gnome-themes-extra
+paru -S --needed --noconfirm cliphist wl-clipboard wlsunset
+systemctl --user enable xwayland-satellite
+
+# APPLICATIONS
+paru -S --needed --noconfirm 1password-beta firefox ghostty nautilus zed
+paru -S --needed --noconfirm gvfs-mtp gvfs-nfs gvfs-smb
