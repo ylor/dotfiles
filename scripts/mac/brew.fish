eab6fa3336fb2cda3b64922d2ee6fb85321a773d
ylor <2200609+ylor@users.noreply.github.com>
2025-11-20 22:17:07 -0500

    I did it for the lulz!

--- scripts/mac/brew.fish
+++ scripts/mac/brew.fish
@@ -1,4 +1,4 @@
-brew update --quiet
+dfs-spin --title "brewing..." brew update
 brew upgrade --quiet
 
 set --export HOMEBREW_BUNDLE_FILE "$DOTFILES_HOME/.config/homebrew/Brewfile"
@@ -9,3 +9,5 @@ if [ $DOTFILES_MODE = full ]
     set --export HOMEBREW_BUNDLE_FILE "$DOTFILES_HOME/.config/homebrew/Caskfile"
     brew bundle --quiet
 end
+
+dfs-success "brewed!"
