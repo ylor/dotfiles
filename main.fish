357291eaca7d7063b566dd848d5b8b06e7a033d3
ylor <2200609+ylor@users.noreply.github.com>
2025-11-21 22:57:11 -0500

    I immediately regret this commit.

--- main.fish
+++ main.fish
@@ -1,16 +1,11 @@
 set -Ux DOTFILES (realpath (status dirname))
 set -Ux DOTFILES_HOME "$DOTFILES/home"
-
-fish_add_path "$DOTFILES/bin" "$DOTFILES/home/.local/bin"
+fish_add_path "$DOTFILES/bin" #"$DOTFILES/home/.local/bin"
 
 clear
-dfs-show-art
-
-test -n "$DOTFILES_MODE" || dfs-mode
-
+test -z "$DOTFILES_MODE" && dfs-mode || dfs-show-art
 set kernel (uname | string lower)
-source "scripts/$kernel.fish" 
+source "scripts/$kernel.fish"
 dfs-spin --title "linking..." dfs-sync
 dfs-success "dotfiles linked!"
-
 dfs-npc "✈ $(set_color --italic)SEE YOU SPACE COWBOY…"
