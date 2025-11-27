2b639981e8120cf024d9e11f56d3dc6b4e117bcd
ylor <2200609+ylor@users.noreply.github.com>
2025-11-27 17:49:33 -0500

    Committed some changes

--- main.fish
+++ main.fish
@@ -5,7 +5,7 @@ fish_add_path "$DOTFILES/bin" #"$DOTFILES/home/.local/bin"
 clear
 test -z "$DOTFILES_MODE" && dfs-mode || dfs-show-art
 set kernel (uname | string lower)
-source "scripts/$kernel.fish"
+[ "$DOTFILES_MODE" = "full" ] && source "scripts/$kernel.fish"
 dfs-spin --title "linking..." dfs-sync
 dfs-success "dotfiles linked!"
 dfs-npc "✈ $(set_color --italic)SEE YOU SPACE COWBOY…"
