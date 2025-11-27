6533a3b4bee01fe43d4162b8c9277596d19703d2
ylor <2200609+ylor@users.noreply.github.com>
2025-11-26 22:36:12 -0500

    arrgghh... damn this thing for not working.

--- home/.config/fish/prompt/fish_right_prompt.fish
+++ home/.config/fish/prompt/fish_right_prompt.fish
@@ -1,5 +1,5 @@
 function fish_right_prompt
-    set machine (string split -m1 -f1 - $hostname)
+    set -q machine || set -g machine (string split -m1 -f1 . $hostname)
     # set symbol ""
     # set symbol "⌘"
     # set -q SSH_CONNECTION SSH_TTY &&
