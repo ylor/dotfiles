c9d86eaae2235299470d58e7129537ab9f20aaa7
ylor <2200609+ylor@users.noreply.github.com>
2025-12-04 14:25:33 -0500

    Chuck Norris Emailed Me This Patch... I'm Not Going To Question It

--- home/.config/hypr/border.sh
+++ home/.config/hypr/border.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
+sleep 1
 COLOR_PRIMARY=$(cat "$HOME/.config/noctalia/colors.json" | jq -r '.mPrimary' | cut -c2- | sed -e 's/^/0xee/')
 COLOR_SECONDARY=$(cat "$HOME/.config/noctalia/colors.json" | jq -r '.mTertiary' | cut -c2- | sed -e 's/^/0xee/')
 hyprctl keyword general:col.active_border $COLOR_PRIMARY $COLOR_SECONDARY 45deg
+# notify-send "foo"
