6f39ad1f81ddde76d4b033515bf2657e37ce890f
ylor <2200609+ylor@users.noreply.github.com>
2025-12-05 11:29:18 -0500

    I'm pretty sure Kelan isn't real and they are just 3 racoons in a trench coat.

--- scripts/linux.fish
+++ scripts/linux.fish
@@ -1,7 +1,7 @@
 # SETUP
 set distro (cat /etc/os-release | grep '^ID=' | cut -d= -f2)
 
-source scripts/$distro.fish
+# source scripts/$distro.fish
 
 # POWER MANAGEMENT
 if upower --enumerate | grep BAT
@@ -17,7 +17,7 @@ end
 for eth in (nmcli --terse connection show --active | grep ethernet | cut -d ":" -f2)
     nmcli connection modify "$eth" 802-3-ethernet.wake-on-lan magic
 end
-sudo systemctl enable bluetooth.service
+# sudo systemctl enable bluetooth.service
 
 # FIREWALL
 
@@ -26,12 +26,12 @@ sudo systemctl enable bluetooth.service
 # sbctl
 
 # GNOME
-if command -vq gsettings
-    gsettings set org.gnome.desktop.wm.preferences button-layout :
-    gsettings set org.gnome.desktop.interface font-name 'Iosevka Aile 11'
-    gsettings set org.gnome.desktop.interface icon-theme 'breeze'
-    gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMono Nerd Font 11'
-end
+# if command -vq gsettings
+#     gsettings set org.gnome.desktop.wm.preferences button-layout :
+#     gsettings set org.gnome.desktop.interface font-name 'Iosevka Aile 11'
+#     gsettings set org.gnome.desktop.interface icon-theme 'breeze'
+#     gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMono Nerd Font 11'
+# end
 
 # DESKTOP
 # TODO: set firefox fonts, userjs, extensions
