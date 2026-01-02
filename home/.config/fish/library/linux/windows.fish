5b8ccb6b3bc49c11f37478ea397829d0ee1a424a
ylor <2200609+ylor@users.noreply.github.com>
2026-01-02 14:53:44 -0500

    Revert "just testing, remember to revert"

--- home/.config/fish/library/linux/windows.fish
+++ home/.config/fish/library/linux/windows.fish
@@ -3,7 +3,7 @@ function windows
     #     echo "$(whoami) ALL=(root) NOPASSWD: /usr/bin/efibootmgr -n *" | sudo tee "/etc/sudoers.d/efibootmgr"
     # end
     
-    set entry (efibootmgr | grep -i windows | string sub --start 5 --end 8)
+    set entry (efibootmgr | grep -i windows | head -n1 | string sub --start 5 --end 8)
     sudo efibootmgr -n $entry || exit 1
     systemctl reboot
 end
