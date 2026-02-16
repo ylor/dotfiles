d26bca6b372f2b1ae16f4d6b1492b623ed24a310
ylor <2200609+ylor@users.noreply.github.com>
2026-02-16 17:57:43 -0500

    formatted all

--- home/.config/hypr/bin/sunshine.sh
+++ home/.config/hypr/bin/sunshine.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # hyprctl output create headless sunshine
-hyprctl keyword monitor sunshine,"${SUNSHINE_CLIENT_WIDTH-3840}"x"${SUNSHINE_CLIENT_HEIGHT-2160}"@"${SUNSHINE_CLIENT_FPS-60}",4000x0,2
+hyprctl keyword monitor sunshine,"${SUNSHINE_CLIENT_WIDTH-3840}"x"${SUNSHINE_CLIENT_HEIGHT-2160}"@"${SUNSHINE_CLIENT_FPS-60}",4000x0,1
 # for m in $(hyprctl monitors -j | jq -r '.[] | .name' | grep -v sunshine); do
 #        hyprctl keyword monitor "$m,disabled"
 # done
@@ -9,3 +9,4 @@ hyprctl dispatch workspace 9
 hyprctl dispatch focusmonitor sunshine
 # hyprctl keyword monitor DP-1,disabled
 # hyprctl output remove sunshine
+#systemd-inhibit --what=idle 
