fa34f8e1a916315160c9f6c22af7b929ce4618ce
ylor <2200609+ylor@users.noreply.github.com>
2026-02-27 13:48:19 -0500

    trying to do something right!

--- home/.config/hammerspoon/lib/app/finder.lua
+++ home/.config/hammerspoon/lib/app/finder.lua
@@ -1,13 +1,24 @@
 ---@diagnostic disable-next-line: undefined-global
 local hs = hs
 
-hs.hotkey.bind({ "cmd" }, "L", function()
-    local app = hs.application.frontmostApplication()
+-- Define a modal group for Finder-specific shortcuts
+local finderKeys = hs.hotkey.modal.new()
 
-    if app:title() == "Finder" then
-        app:selectMenuItem({ "Go", "Go to Folder…" })
-    else
-        -- Optional: allow Cmd+L to work normally in other apps
-        hs.eventtap.keyStroke({ "cmd" }, "L")
+-- Bind your key to the modal instead of the global scope
+finderKeys:bind({ "cmd" }, "l", function()
+    local finder = hs.application.get("Finder")
+    finder:selectMenuItem({ "Go", "Go to Folder…" })
+end)
+
+-- Use a watcher to enable/disable the modal
+finderWatcher = hs.application.watcher.new(function(appName, eventType, appObject)
+    if appName == "Finder" then
+        if eventType == hs.application.watcher.activated then
+            finderKeys:enter()
+        elseif eventType == hs.application.watcher.deactivated then
+            finderKeys:exit()
+        end
     end
 end)
+
+finderWatcher:start()
