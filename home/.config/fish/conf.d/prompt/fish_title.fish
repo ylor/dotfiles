f67e55bd04edfca36bc86928277e9c99cff1af26
ylor <2200609+ylor@users.noreply.github.com>
2026-03-11 10:57:05 -0400

    Errare humanum est.

--- home/.config/fish/conf.d/prompt/fish_title.fish
+++ home/.config/fish/conf.d/prompt/fish_title.fish
@@ -1,13 +1,11 @@
 function fish_title
+    set -q SSH_CLIENT SSH_TTY && echo "[$(prompt_hostname)] "
+
     if [ $PWD = $HOME ]
         echo 👻
+    else if set -q argv[1]
+        echo $argv
     else
-        set -q SSH_CLIENT SSH_TTY && echo "[$(prompt_hostname)]"
-
-        if set -q argv[1]
-            echo $argv
-        else
-            string match -r '[^/]*$' $PWD
-        end
+        string match -r '[^/]*$' $PWD
     end
 end
