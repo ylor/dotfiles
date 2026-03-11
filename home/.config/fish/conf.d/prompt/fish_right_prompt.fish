b02baaa202b619343fb881e7982b813342d580d2
ylor <2200609+ylor@users.noreply.github.com>
2026-03-11 14:40:18 -0400

    pep8 - cause I fell like doing a barrel roll

--- home/.config/fish/conf.d/prompt/fish_right_prompt.fish
+++ home/.config/fish/conf.d/prompt/fish_right_prompt.fish
@@ -1,12 +1,12 @@
-function fish_right_prompt
-    if not set -q machine
-        set -g machine (string split -m1 -f1 . $hostname)
-        string match -r "^PAPA" "$machine" && set machine papa
-    end
+# function fish_right_prompt
+#     if not set -q machine
+#         set -g machine (string split -m1 -f1 . $hostname)
+#         string match -r "^PAPA" "$machine" && set machine papa
+#     end
 
-    # set symbol ""
-    # set symbol "⌘"
-    # █ ▓ ▒ ░ ⣿
-    set_color brblack
-    string lower "$USER@$machine ✦"
-end
+#     # set symbol ""
+#     # set symbol "⌘"
+#     # █ ▓ ▒ ░ ⣿
+#     set_color brblack
+#     string lower "$USER@$machine ✦"
+# end
