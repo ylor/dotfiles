d023c5f5975a5921062572e8a8aa1daa5d60fbe0
ylor <2200609+ylor@users.noreply.github.com>
2026-04-07 22:19:18 -0400

    So my boss wanted this button ...

--- home/.config/fish/conf.d/brew.fish
+++ home/.config/fish/conf.d/brew.fish
@@ -23,22 +23,19 @@ if command -vq brew # https://github.com/Homebrew/brew
                 command brew uninstall $args
             case re
                 command brew reinstall $args
-            case s
-                command brew search $args
+            case search s
+                if command -vq tv
+                    set -l tv_args --preview-command 'brew info {1}'
+                    test -n "$args[1]"; and set -a tv_args --input "$args[1]"
+                    set -l selection (begin; command brew formulae; command brew casks; end | sort | tv $tv_args)
+                    test -n "$selection"; and command brew install $selection
+                else
+                    command brew search $args
+                end
             case up
                 command brew update && command brew upgrade
-                # detach fish_update_completions
-            case formula
-                command brew formulae | fzf --multi --layout reverse-list --preview 'brew info {1}' | xargs -ro brew install
-            case casks
-                command brew casks | fzf --multi --layout reverse-list --preview 'brew info {1}' | xargs -ro brew install
             case '*'
                 command brew $argv
         end
     end
 end
-
-# if command -vq fzf
-#     alias formula="brew formulae | fzf --multi --layout reverse-list --preview 'brew info {1}' | xargs -ro brew install"
-#     alias casks="brew casks | fzf --multi --layout reverse-list --preview 'brew info {1}' | xargs -ro brew install"
-# end
