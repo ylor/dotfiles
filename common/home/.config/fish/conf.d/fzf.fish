73f551e4b506489d34b0a2d69d6b0107bbd3385d
ylor <2200609+ylor@users.noreply.github.com>
2026-05-13 20:20:16 -0400

    I will run 'cargo fmt' before committing.

--- common/home/.config/fish/conf.d/fzf.fish
+++ common/home/.config/fish/conf.d/fzf.fish
@@ -1,6 +1,13 @@
-# if command -q fzf # https://github.com/junegunn/fzf - fuzzy finder
+if command -q fzf # https://github.com/junegunn/fzf - fuzzy finder
+    set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS
+      --color=fg:-1,fg+:#d0d0d0,bg:-1,bg+:#262626
+      --color=hl:#5f87af,hl+:#5fd7ff,info:#d0d0d0,marker:#d0d0d0
+      --color=prompt:#d0d0d0,spinner:#d0d0d0,pointer:#d0d0d0,header:#d0d0d0
+      --color=border:#262626,label:#aeaeae,query:#d9d9d9
+      --border=\"rounded\" --border-label=\"\" --preview-window=\"border-rounded\" --prompt=\"→ \"
+      --marker=\"›\" --pointer=\"◆\" --separator=\"─\" --scrollbar=\"│\""
 #     fzf --fish | source
 
 #     alias fcd="fzf --preview 'bat --style=numbers --color=always {}'"
 #     alias ff="fzf --preview 'bat --style=numbers --color=always {}'"
-# end
+end
