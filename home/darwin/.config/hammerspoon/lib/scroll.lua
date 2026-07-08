300238b9751c743530a63fdddbb7db8554eca72b
ylor <2200609+ylor@users.noreply.github.com>
2026-07-08 15:42:52 -0400

    bla

---
+++ home/darwin/.config/hammerspoon/lib/scroll.lua
@@ -0,0 +1,13 @@
+local hs = hs ---@diagnostic disable-line: undefined-global
+
+_G.MouseScrollReverser = hs.eventtap.new({ hs.eventtap.event.types.scrollWheel }, function(e)
+    local p = hs.eventtap.event.properties
+    if e:getProperty(p.eventSourceStateID) ~= 1 then
+        return false -- not real hardware input, leave it alone
+    end
+    if e:getProperty(p.scrollWheelEventIsContinuous) == 0 then
+        e:setProperty(p.scrollWheelEventDeltaAxis1,
+            -e:getProperty(p.scrollWheelEventDeltaAxis1))
+    end
+    return false
+end):start()
