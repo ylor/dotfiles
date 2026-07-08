1545653dada4ee273cb765c315582dc281ebfb3f
ylor <2200609+ylor@users.noreply.github.com>
2026-07-08 15:34:21 -0400

    Never before had a small typo like this one caused so much damage.

---
+++ home/darwin/.config/fish/functions/ghostty-tip.fish
@@ -0,0 +1,7 @@
+function ghostty-tip
+    set file $HOME/.config/ghostty/local
+    set line "auto-update-channel = tip"
+
+    mkdir -p (dirname $file)
+    grep --quiet --no-messages --fixed-strings --line-regexp -- $line $file; or echo $line >> $file
+end
