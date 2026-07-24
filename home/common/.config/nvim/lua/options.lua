5d7b7ccb52b02b2121fb2b9c85e1210dd2166373
ylor <2200609+ylor@users.noreply.github.com>
2026-07-24 09:35:38 -0400

    fix bug, for realz

--- home/common/.config/nvim/lua/options.lua
+++ home/common/.config/nvim/lua/options.lua
@@ -4,8 +4,8 @@ vim.loader.enable()
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
@@ -16,59 +16,62 @@ vim.g.have_nerd_font = true
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
-vim.o.number = true
+vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.o.relativenumber = true
+vim.opt.relativenumber = true
+vim.o.statuscolumn = "%{v:relnum == 0 ? v:lnum : v:relnum + 1}%="
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
+vim.opt.showmode = false
 
 -- Show the current buffer in the terminal title.
-vim.o.title = true
-vim.o.titlestring = 'nvim - %t'
+vim.opt.title = true
+vim.opt.titlestring = "nvim - %t"
 
 -- Don't show Neovim's built-in intro screen on startup.
-vim.opt.shortmess:append 'I'
+vim.opt.shortmess:append("I")
 
 -- Keep the command row at Neovim's startup-default height. Changing this to
 -- zero during initialization resizes the editing grid and causes a visible
 -- one-row layout jump in some terminals.
-vim.o.cmdheight = 1
+vim.opt.cmdheight = 1
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
 
 -- Enable break indent
-vim.o.breakindent = true
+vim.opt.breakindent = true
 
 -- Enable undo/redo changes even after closing and reopening a file
 -- vim.cmd("packadd nvim.undotree")
 -- vim.keymap.set("n", "<leader>u", require("undotree").open)
-vim.o.undofile = true
+vim.opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Decrease update time
-vim.o.updatetime = 250
+vim.opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
-vim.o.timeoutlen = 500
+vim.opt.timeoutlen = 500
 
 -- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
@@ -78,34 +81,34 @@ vim.o.splitbelow = true
 --  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
 --   See `:help lua-options`
 --   and `:help lua-guide-options`
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
-vim.o.cursorline = true
+vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+vim.opt.scrolloff = 10
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
-vim.o.confirm = true
+vim.opt.confirm = true
 
 -- Reload a buffer automatically when the underlying file changed on disk,
 -- as long as it hasn't been modified in the buffer itself.
 -- See `:help 'autoread'`
-vim.o.autoread = true
+vim.opt.autoread = true
 
 -- Remove the "How-to disable mouse" entry (and its separator) that Nvim adds
 -- to the default right-click popup menu. See `:h vim_diff.txt` for this exact
 -- recipe.
-vim.cmd [[
+vim.cmd([[
   aunmenu PopUp.How-to\ disable\ mouse
   aunmenu PopUp.-2-
-]]
+]])
 
 -- vim: ts=2 sts=2 sw=2 et
