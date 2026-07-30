2fadb32ce016b67b0748e7e9596bc98531ccbefa
ylor <2200609+ylor@users.noreply.github.com>
2026-07-30 17:33:35 -0400

    I'm just a grunt. Don't blame me for this awful PoS.

--- home/common/.config/fish/conf.d/yazi.fish
+++ home/common/.config/fish/conf.d/yazi.fish
@@ -1,8 +1,10 @@
-function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    command yazi $argv --cwd-file="$tmp"
-    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
-        builtin cd -- "$cwd"
+if command -q yazi
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+            builtin cd -- "$cwd"
+        end
+        command rm -f -- "$tmp"
     end
-    command rm -f -- "$tmp"
 end
