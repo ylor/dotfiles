ef55b565ab1e1bf44aae2d57a14ca0fa5bae0e4e
ylor <2200609+ylor@users.noreply.github.com>
2026-09-15 16:18:03 -0400

    GIT :/

--- home/base/.config/nvim/lua/config/options.lua
+++ home/base/.config/nvim/lua/config/options.lua
@@ -1,5 +1,12 @@
--- Options are automatically loaded before lazy.nvim startup.
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
 require("config.remote_clipboard").setup()
 
 vim.opt.relativenumber = false
+vim.opt.signcolumn = "auto"
 vim.g.autoformat = false
+
+vim.opt.wrap = true
+vim.opt.textwidth = 100
+vim.opt.colorcolumn = "100"
